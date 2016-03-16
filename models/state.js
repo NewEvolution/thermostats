662257efb3c968060c28d8468128f086b979b120
Ryan Tanay <ryantanay@newevolution.org>
2016-03-16 10:04:36 -0500

    delinted the state model

--- models/state.js
+++ models/state.js
@@ -1,12 +1,13 @@
 'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var state = sequelize.define('state', {
-    name: DataTypes.VARCHAR(20),
-    abbr: DataTypes.VARCHAR(2)
+
+module.exports = (sequelize, DataTypes) => {
+  const state = sequelize.define('state', {
+    name: DataTypes.VARCHAR(20), // eslint-disable-line no-magic-numbers
+    abbr: DataTypes.VARCHAR(2) // eslint-disable-line no-magic-numbers
   }, {
     classMethods: {
-      associate: function(models) {
-        // associations can be defined here
+      associate: (models) => {
+        state.hasMany(models.area);
       }
     }
   });
