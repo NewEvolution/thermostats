fd74e14fa8c3223be12b3f52839f3ca7820bcdd8
Ryan Tanay <ryantanay@newevolution.org>
2016-04-19 15:58:30 -0500

    Modified map display to move the render inside the get reply & populate the map with the returned data

--- public/controllers/country.js
+++ public/controllers/country.js
@@ -23,26 +23,39 @@ app.controller('CountryCtrl', ['$http', function ($http) {
     });
   }
 
-  this.temps = [];
-
-  $(function () {
-    new jvm.MultiMap({ // eslint-disable-line no-new
-      container: $('#map'),
-      maxLevel: 1,
-      main: {
-        map: 'us_lcc_en'
-      },
-      mapUrlByCode: function (code, multiMap) {
-        return '/vendor/jvectormap/tests/assets/us/jquery-jvectormap-data-' +
-               code.toLowerCase() + '-' +
-               multiMap.defaultProjection + '-en.js';
-      }
-    });
-  });
-
-  $http.get('/api')
+  $http.get('/api/summary')
   .then(function (response) {
-    this.temps = response.data;
-    console.log('this.temps: ', this.temps);
+    var rawtemps = response.data;
+    var temps = {};
+    for (var i = rawtemps.length - 1; i >= 0; --i) { // eslint-disable-line no-magic-numbers
+      temps['US-' + rawtemps[i].abbr] = rawtemps[i].data.heat;
+    }
+    console.log("temps", temps);
+    $(function () {
+      new jvm.MultiMap({ // eslint-disable-line no-new
+        container: $('#map'),
+        maxLevel: 1,
+        main: {
+          map: 'us_lcc_en',
+          series: {
+            regions: [{
+              values: temps,
+              scale: ['#500000', '#ff0000']
+            }]
+          },
+          onRegionTipShow: function (e, el, name) {
+            var heat = temps[name];
+            if (heat) {
+              el.html(el.html() + ': Heat - ' + heat);
+            }
+          }
+        },
+        mapUrlByCode: function (code, multiMap) {
+          return '/vendor/jvectormap/tests/assets/us/jquery-jvectormap-data-' +
+                 code.toLowerCase() + '-' +
+                 multiMap.defaultProjection + '-en.js';
+        }
+      });
+    });
   });
 }]);
